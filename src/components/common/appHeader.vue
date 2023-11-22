60213f454f97be6ec03a5ebcd4413644fe16ee1b
esmanur-uzun <esmabitmez@gmail.com>
2023-11-22 16:19:40 +0300

    worked on header

--- src/components/common/appHeader.vue
+++ src/components/common/appHeader.vue
@@ -1,77 +1,185 @@
 <template>
   <div>
-    <nav>
-      <div>
+    <nav class="container-fluid main-menu">
+      <div class="container d-flex justify-content-around">
         <ul>
           <li>
             <a href="">
               Kadın
-              <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z"/></svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="0.8em"
+                viewBox="0 0 320 512"
+              >
+                <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                <path
+                  d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z"
+                />
+              </svg>
             </a>
-            <ul>
+            <ul class="dropmenu">
               <li><a href="">deneme</a></li>
               <li><a href="">deneme</a></li>
               <li><a href="">deneme</a></li>
               <li><a href="">deneme</a></li>
-
             </ul>
           </li>
           <li>
             <a href="">
               Erkek
-              <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z"/></svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="0.8em"
+                viewBox="0 0 320 512"
+              >
+                <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                <path
+                  d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z"
+                />
+              </svg>
             </a>
-            <ul>
+            <ul class="dropmenu">
               <li><a href="">deneme</a></li>
               <li><a href="">deneme</a></li>
               <li><a href="">deneme</a></li>
               <li><a href="">deneme</a></li>
-
             </ul>
           </li>
           <li>
             <a href="">
               Anne&Çocuk
-              <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z"/></svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="0.8em"
+                viewBox="0 0 320 512"
+              >
+                <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                <path
+                  d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z"
+                />
+              </svg>
             </a>
-            <ul>
+            <ul class="dropmenu">
               <li><a href="">deneme</a></li>
               <li><a href="">deneme</a></li>
               <li><a href="">deneme</a></li>
               <li><a href="">deneme</a></li>
-
+            </ul>
+          </li>
+          <li>
+            <a href="">
+              Kozmetik
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="0.8em"
+                viewBox="0 0 320 512"
+              >
+                <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                <path
+                  d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z"
+                />
+              </svg>
+            </a>
+            <ul class="dropmenu">
+              <li><a href="">deneme</a></li>
+              <li><a href="">deneme</a></li>
+              <li><a href="">deneme</a></li>
+              <li><a href="">deneme</a></li>
             </ul>
           </li>
           <li>
             <a href="">
               Ev&Yaşam
-              <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z"/></svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="0.8em"
+                viewBox="0 0 320 512"
+              >
+                <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                <path
+                  d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z"
+                />
+              </svg>
             </a>
-            <ul>
+            <ul class="dropmenu">
               <li><a href="">deneme</a></li>
               <li><a href="">deneme</a></li>
               <li><a href="">deneme</a></li>
               <li><a href="">deneme</a></li>
-
             </ul>
           </li>
           <li>
             <a href="">
               Elektronik
-              <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z"/></svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="0.8em"
+                viewBox="0 0 320 512"
+              >
+                <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                <path
+                  d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z"
+                />
+              </svg>
             </a>
-            <ul>
+            <ul class="dropmenu">
               <li><a href="">deneme</a></li>
               <li><a href="">deneme</a></li>
               <li><a href="">deneme</a></li>
               <li><a href="">deneme</a></li>
-
             </ul>
           </li>
-
         </ul>
       </div>
     </nav>
+    <div class="menu-toggle">
+      <button
+        class="btn"
+        type="button"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasExample"
+        aria-controls="offcanvasExample"
+      >
+      <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
+      </button>
+
+      <div
+        class="offcanvas offcanvas-start"
+        tabindex="-1"
+        id="offcanvasExample"
+        aria-labelledby="offcanvasExampleLabel"
+      >
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="offcanvas-body">
+          <div>
+            Some text as placeholder. In real life you can have the elements you
+            have chosen. Like, text, images, lists, etc.
+          </div>
+          <div class="dropdown mt-3">
+            <button
+              class="btn btn-secondary dropdown-toggle"
+              type="button"
+              data-bs-toggle="dropdown"
+            >
+              Dropdown button
+            </button>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -80,9 +188,49 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
-*{
+* {
   padding: 0;
   margin: 0;
   box-sizing: border-box;
+}
+ul {
+  list-style: none;
+
+  li {
+    display: inline-block;
+    position: relative;
+    a {
+      display: block;
+      padding: 5px 25px;
+      text-decoration: none;
+      text-align: center;
+      svg {
+        fill: aqua;
+      }
+    }
+    .dropmenu {
+      width: 100%;
+      position: absolute;
+      z-index: 999;
+      display: none;
+      li {
+        display: block;
+      }
+    }
+    &:hover .dropmenu {
+      display: block;
+    }
+  }
+}
+.menu-toggle{
+  display: none;
+}
+@media (max-width: 768px) {
+  .main-menu {
+    display: none;
+  }
+  .menu-toggle{
+    display: block;
+  }
 }
 </style>
