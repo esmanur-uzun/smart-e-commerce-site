8dcdf9f2cdeb161ba05acf2ce564a7e44c131f2e
esmanur-uzun <esmabitmez@gmail.com>
2023-12-07 16:42:32 +0300

    worked on mainPage

---
+++ src/components/common/productSlider.vue
@@ -0,0 +1,50 @@
+<script>
+import { Swiper, SwiperSlide } from "swiper/vue";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Pagination, Navigation } from "swiper/modules";
+import ProductCard from './productCard.vue';
+export default {
+    components: {
+    Swiper,
+    SwiperSlide,
+    ProductCard
+  },
+  setup() {
+    return {
+      modules: [Pagination, Navigation],
+    };
+  },
+};
+</script>
+<template>
+  <div>
+    <swiper
+      ref="{swiperRef}"
+      :slidesPerView="1"
+      :spaceBetween="20"
+      :navigation="true"
+      :breakpoints="{
+        '640': {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        '768': {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+        '1024': {
+          slidesPerView: 4,
+          spaceBetween: 50,
+        },
+      }"
+      :modules="modules"
+      class="mySwiper"
+    >
+      <swiper-slide v-for="index in 10" :key="index">
+        <product-card></product-card>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -6,12 +6,16 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import { Pagination, Navigation } from "swiper/modules";
+import ProductSlider from "../components/common/productSlider.vue";
+import AppFooter from '../components/common/appFooter.vue';
 export default {
   components: {
     Swiper,
     SwiperSlide,
     appHeader,
     ProductCard,
+    ProductSlider,
+    AppFooter,
   },
   setup() {
     return {
@@ -24,7 +28,7 @@ export default {
   <div>
     <app-header></app-header>
     <div class="main-container">
-      <div class="container">
+      <div class="container-fluid">
         <div class="row d-flex justify-content-center">
           <div class="col-lg-5">
             <div class="d-discount">
@@ -75,59 +79,92 @@ export default {
         </div>
       </div>
       <div class="brands container mt-5">
+        <!-- <button class="swiper-buttons-next" @click="swiper.slideNext()">Slide to the next slide</button> -->
+        <span><h2>Aradığın Markalar</h2></span>
         <swiper
-          ref="{swiperRef}"
-          :slidesPerView="4"
-          :spaceBetween="20"
+          :slidesPerView="1"
+          :spaceBetween="10"
           :navigation="true"
           :breakpoints="{
-            '640': {
+            '@0.00': {
+              slidesPerView: 2,
+              spaceBetween: 10,
+            },
+            '@0.75': {
               slidesPerView: 2,
               spaceBetween: 20,
             },
-            '768': {
+            '@1.00': {
               slidesPerView: 4,
               spaceBetween: 40,
             },
-            '1024': {
-              slidesPerView: 5,
+            '@1.50': {
+              slidesPerView: 7,
               spaceBetween: 50,
             },
           }"
           :modules="modules"
-          class="mySwiper"
+          class="mySwiper mt-3"
         >
-          <swiper-slide v-for="index in 10" :key="index">
-            <product-card></product-card>
+          <swiper-slide v-for="i in 10" :key="i">
+            <div class="brand-box">
+              <img src="../assets/medias/photos/format_webp.jfif" alt="" />
+            </div>
           </swiper-slide>
-          
         </swiper>
-
-        <!-- <swiper-container class="mySwiper text-center" navigation="true">
-          <swiper-slide>
-            <product-card></product-card>
-          </swiper-slide>
-          <swiper-slide>
-            <product-card></product-card>
-          </swiper-slide>
-          <swiper-slide>
-            <product-card></product-card>
-          </swiper-slide>
-          <swiper-slide>
-            <product-card></product-card>
-          </swiper-slide>
-          <swiper-slide>
-            <product-card></product-card>
-          </swiper-slide>
-          <swiper-slide>
-            <product-card></product-card>
-          </swiper-slide>
-        </swiper-container> -->
       </div>
+      <div class="container-fluid mt-5 mb-5">
+        <div class="background">
+          <img
+            src="../assets/medias/photos/amy-shamblen-F52I5BtDuhY-unsplash.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+      <div class="container mt-5">
+        <span class="d-flex align-items-center">
+          <h2>Premium Fırsatları</h2>
+          <a class="ps-3 see-more" href="">
+            Tümü
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="10"
+              width="10"
+              viewBox="0 0 320 512"
+            >
+              <path
+                d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"
+              />
+            </svg>
+          </a>
+        </span>
+        <product-slider></product-slider>
+      </div>
+      <div class="container m_100">
+        <span>
+          <h2>Akıllı Telefon</h2>
+          <a class="ps-3 see-more" href="">
+            Tümü
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="10"
+              width="10"
+              viewBox="0 0 320 512"
+            >
+              <path
+                d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"
+              />
+            </svg>
+          </a>
+        </span>
+        <product-slider></product-slider>
+      </div>
+      <app-footer></app-footer>
     </div>
   </div>
 </template>
 <style lang="scss" scoped>
+$text-color: #403d39;
 .d-discount {
   height: 60vh;
   background-image: linear-gradient(to bottom right, #ff0000, #ff6700, #f48c06);
@@ -168,5 +205,40 @@ export default {
     height: 250px;
     object-fit: cover;
   }
+}
+.brands {
+  .brand-box {
+    width: 120px;
+    height: 60px;
+    border: 1px solid rgb(199, 199, 199);
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
+.background {
+  width: 100%;
+  height: 120px;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+}
+h2 {
+  color: $text-color;
+  font-weight: bold;
+  
+}
+.see-more{
+  text-decoration: none;
+  color: #f48c06;
+  svg{
+    fill: #ff6700;
+  }
+}
+.m_100{
+  margin-top: 100px;
 }
 </style>
