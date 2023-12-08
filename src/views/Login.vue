af2da0aa544abc35c00d62462b61dfc5bd9c841b
esmanur-uzun <esmabitmez@gmail.com>
2023-12-08 10:44:56 +0300

    design of the login components started

---
+++ src/views/Login.vue
@@ -0,0 +1,13 @@
+<script>
+import appHeader from '../components/common/appHeader.vue'
+export default{
+  components: { appHeader },}
+</script>
+<template>
+    <div>
+        <app-header></app-header>
+    </div>
+</template>
+<style lang="scss" scoped>
+
+</style>
